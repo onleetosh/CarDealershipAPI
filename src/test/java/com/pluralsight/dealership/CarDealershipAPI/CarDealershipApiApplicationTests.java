0f9376ee61c0d713fbb3687bac9b8b26beb98f08
tashi thompson <tosh@tashis-MacBook-Pro.local>
2024-12-11 15:17:31 -0500

    add project'

---
+++ src/test/java/com/pluralsight/dealership/CarDealershipAPI/CarDealershipApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.dealership.CarDealershipAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealershipApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
